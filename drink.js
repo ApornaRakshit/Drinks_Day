6a91a3385a34f1f1cf385b073e99c17adbf32849
Aporna16 <rakshit.aporna@gmail.com>
2025-01-05 23:44:27 +0600

    add new section

--- drink.js
+++ drink.js
@@ -1,3 +1,21 @@
+const searchItem = () => {
+    document.getElementById('item-cont').innerHTML = "";
+    document.getElementById('error-msg').innerHTML = "";
+
+    const searchBox = document.getElementById('search-box');
+    const searchName = searchBox.value;
+    searchBox.value = '';
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchName}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.length == null) {
+                showError();
+            }
+            displayItem(data);
+        });
+}
+
 const loadItems = () => {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
         .then((res) => res.json())
@@ -7,64 +25,53 @@ const loadItems = () => {
 };
 
 const displayItem = (items) => {
-    //console.log(items);
+    console.log(items);
     const itemContainer = document.getElementById("item-cont")
 
     let allItems = Object.values(items);
     allItems.forEach(item => {
-        //console.log(item);
+        console.log(item);
 
         item.forEach(element => {
             const div = document.createElement("div");
-
-            //console.log(element);
+            div.classList.add('col');
             div.innerHTML = `
-            <img class="card-img" src=${element.strDrinkThumb} alt=""/>
-            <h4>Name: ${element.strDrink}</h4>
-            <h6>Category: ${element.strCategory}</h6>
-            <p>Instructions: ${element.strInstructions.slice(0, 15)}...</p>
-
-            <div>
-                <button onclick="handleAddToCart('${element.idDrink}', '${element?.strDrink}')">Add to cart</button>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="getSingleItem('${element.idDrink}')">Details</button>
+        <div class="card rounded-3 w-75 h-100 ms-5">
+            <img src="${element.strDrinkThumb}" class="card-img-top img-fluid mx-auto" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Name: ${element.strDrink}</h5>
+                <h6>Category: ${element.strCategory}</h6>
+                <p>Instructions: ${element.strInstructions.slice(0, 15)}...</p>
+                <p class="card-text"></p>
+                <br>
+                <button class="btn btn-info" onclick="handleAddToCart('${element.idDrink}', '${element?.strDrink}')">Add to cart</button>
+                <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="getSingleItem('${element.idDrink}')">Details</button>
             </div>
+        </div>
             `;
             itemContainer.appendChild(div);
         });
     });
 };
 
+let drinkCount = 1;
 const handleAddToCart = (id, name) => {
-    //console.log(id, name);
+    if (drinkCount > 7) {
+        alert("You can't add more than 7 drinks to the cart.");
+        return; 
+    }
+    console.log(id, name);
     const container = document.getElementById("cart-items");
 
     const div = document.createElement("div");
     div.classList.add("orders")
-    div.innerHTML = `     
-        <h5>1.</h5>
-        <img src="" alt="">
-        <h5>${id}</h5>
+    div.innerHTML = `
+        <h5>${drinkCount}</h5>
         <h5>${name}</h5>
     `;
-    container.appendChild(div)
+    container.appendChild(div);
+    drinkCount++;
 };
-
-const searchItem = () => {
-    
-    const url = ``;
-
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        if (data.length == 0){
-            showError();
-        }
-    else{
-        showSearchResult(data);
-    }
-    });
-}
-//------------------------------------------------------------------------------------------
 
 const getSingleItem = (id) => {
     console.log(id);
@@ -74,14 +81,12 @@ const getSingleItem = (id) => {
 };
 
 const showDetail = (item) => {
-    // console.log(item);
     const itemDetail = document.getElementById("modal-body");
-
     let showItem = Object.values(item);
     showItem.forEach(element => {
-        console.log(element);
+        //console.log(element);
         element.forEach(e => {
-            console.log(e);
+            //console.log(e);
 
             itemDetail.innerHTML = `
             <img class="card-img" src=${e.strDrinkThumb} alt=""/>
@@ -96,4 +101,9 @@ const showDetail = (item) => {
     });
 };
 
+const showError = () => {
+    document.getElementById('error-msg').innerHTML = `
+    <p class="card w-25 border border-warning text-center shadow-lg p-1 bg-body rounded">No item found</p>
+    `;
+}
 loadItems()
